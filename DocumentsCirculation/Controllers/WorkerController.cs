f0b2b78c8ddece5cf2967d886a60a02833bfd956
Николай Журавлев <colya.juravlyov2011@ya.ru>
2019-12-12 01:59:50 +0300

    7 коммит. ДАО+Контроллер+Представления для работы с рабочими

---
+++ DocumentsCirculation/Controllers/WorkerController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DocumentsCirculation.DAO;
+using DocumentsCirculation.Models;
+
+namespace DocumentsCirculation.Controllers
+{
+    public class WorkerController : Controller
+    {
+        WorkerDAO workerDAO = new WorkerDAO();
+        // GET: Worker
+        public ActionResult WorkerIndex()
+        {
+            return View(workerDAO.GetAllWorkers());
+        }
+
+        // GET: Worker/Details/5
+        public ActionResult WorkerDetails(int id)
+        {
+            List<Worker> workerList = workerDAO.GetAllWorkers();
+            int pos = 0;
+            for (int i = 0; i < workerList.Count; i++)
+                if (id == workerList[i].workerID)
+                {
+                    pos = i;
+                }
+            return View(workerList[pos]);
+        }
+
+        // GET: Worker/Create
+        public ActionResult WorkerCreate()
+        {
+            return View();
+        }
+
+        // POST: Worker/Create
+        [HttpPost]
+        public ActionResult WorkerCreate(Worker w)
+        {
+            try
+            {
+                if (workerDAO.AddWorker(w))
+                    return RedirectToAction("WorkerIndex");
+                else return View("WorkerCreate");
+            }
+            catch
+            {
+                return View("WorkerCreate");
+            }
+        }
+
+        // GET: Worker/Edit/5
+        public ActionResult WorkerEdit(int id)
+        {
+            List<Worker> workerList = workerDAO.GetAllWorkers();
+            int pos = 0;
+            for (int i = 0; i < workerList.Count; i++)
+                if (id == workerList[i].workerID)
+                {
+                    pos = i;
+                }
+            return View(workerList[pos]);
+        }
+
+        // POST: Worker/Edit/5
+        [HttpPost]
+        public ActionResult WorkerEdit(int id, Worker w)
+        {
+            try
+            {
+                if (workerDAO.ChangeWorker(id,w))
+                    return RedirectToAction("WorkerIndex");
+                else return View("WorkerEdit");
+            }
+            catch
+            {
+                return View("WorkerEdit");
+            }
+        }
+
+        // GET: Worker/Delete/5
+        public ActionResult WorkerDelete(int id)
+        {
+            List<Worker> workerList = workerDAO.GetAllWorkers();
+            int pos = 0;
+            for (int i = 0; i < workerList.Count; i++)
+                if (id == workerList[i].workerID)
+                {
+                    pos = i;
+                }
+            return View(workerList[pos]);
+        }
+
+        // POST: Worker/Delete/5
+        [HttpPost]
+        public ActionResult WorkerDelete(int id, FormCollection collection)
+        {
+            try
+            {
+                if (workerDAO.DropWorker(id))
+                    return RedirectToAction("WorkerIndex");
+                else return View("WorkerDelete");
+            }
+            catch
+            {
+                return View("WorkerDelete");
+            }
+        }
+    }
+}
---
+++ DocumentsCirculation/DAO/WorkerDAO.cs
@@ -0,0 +1,120 @@
+﻿using DocumentsCirculation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DocumentsCirculation.DAO
+{
+    public class WorkerDAO:DAO
+    {
+        public List<Worker> GetAllWorkers()
+        {
+            Logger.InitLogger();
+            Logger.Log.Info("Метод вызова всех рабочих");
+            Connect();
+            List<Worker> WList = new List<Worker>();
+            try
+            {
+                SqlCommand command = new SqlCommand("select * from Worker", Connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Worker worker = new Worker();
+
+                    worker.workerID = Convert.ToInt32(reader["workerID"]);
+                    worker.fio = Convert.ToString(reader["fio"]);
+                    worker.birthdate = Convert.ToDateTime(reader["birthdate"]);
+                    worker.employdate = Convert.ToDateTime(reader["employdate"]);
+                    worker.salary = Convert.ToDecimal(reader["salary"]);
+                    worker.role = Convert.ToString(reader["role"]);
+
+                    WList.Add(worker);
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error("ERROR: " + e.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+            return WList;
+        }
+
+        public bool AddWorker(Worker worker)
+        {
+            bool result = true;
+            Connect();
+
+            try
+            {
+                SqlCommand add = new SqlCommand("insert into Worker (fio, birthdate, employdate, salary, role) "
+                    + "VALUES (@fio, @birthdate, @employdate, @salary, @role)", Connection);
+
+                add.Parameters.Add(new SqlParameter("@fio", worker.fio));
+                add.Parameters.Add(new SqlParameter("@birthdate", worker.birthdate));
+                add.Parameters.Add(new SqlParameter("@employdate", worker.employdate));
+                add.Parameters.Add(new SqlParameter("@salary", worker.salary));
+                add.Parameters.Add(new SqlParameter("@role", worker.role));
+
+                add.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error("ERROR: " + e.Message);
+                result = false;
+            }
+            finally { Disconnect(); }
+            return result;
+        }
+
+        public bool DropWorker(int id)
+        {
+            bool result = true;
+            Connect();
+
+            try
+            {
+                string dropstring = string.Format("Delete from Worker where worklerID='{0}'", id);
+                SqlCommand dropclient = new SqlCommand(dropstring, Connection);
+
+                dropclient.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error("ERROR: " + e.Message);
+                result = false;
+            }
+            finally { Disconnect(); }
+            return result;
+        }
+
+        public bool ChangeWorker(int id, Worker worker)
+        {
+            bool result = true;
+            Connect();
+
+            try
+            {
+                string updatestring = string.Format("update Worker set fio=@fio, birthdate=@birthdate, employdate=@employdate, salary=@salary, role=@role where workerID='{0}'", id);
+
+                SqlCommand change = new SqlCommand(updatestring, Connection);
+
+                change.Parameters.AddWithValue("@fio", worker.fio);
+                change.Parameters.AddWithValue("@birthdate", worker.birthdate);
+                change.Parameters.AddWithValue("@employdate", worker.employdate);
+                change.Parameters.AddWithValue("@salary", worker.salary);
+                change.Parameters.AddWithValue("@role", worker.role);
+                change.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            finally { Disconnect(); }
+            return result;
+        }
+    }
+}
